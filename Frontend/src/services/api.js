529e90f568c68465b6310cac4516134220304556
VanshSUTHAR <sutharvansh022@gmail.com>
2026-06-15 12:04:20 +0530

    change

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -4,7 +4,7 @@ import storage from '../utils/storage';
 const API_URL = process.env.REACT_APP_API_URL || '';
 
 const api = axios.create({
-  baseURL: `${API_URL}/api`,
+  baseURL: `${API_URL}api`,
   headers: {
     'Content-Type': 'application/json',
   },
